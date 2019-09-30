062adfbe399a57ecaab631adf792780aa96ad816
Adam Finch <atyreefinch@gmail.com>
2019-09-30 16:08:20 -0400

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,14 @@
+require 'bundler/setup'
+Bundler.require
+# set :database_file, "./database.yml"
+
+# require 'sinatra/activerecord'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/development.sqlite"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
